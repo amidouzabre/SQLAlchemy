1e8fc7be05f3bc7cf2a9bfaa93fbec9891a1a8bc
“Amidou <amidou.zabre@gmail.com>
2024-09-14 10:18:36 +0000

    models.py: create warehouse table structure

--- models.py
+++ models.py
@@ -2,3 +2,6 @@ from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
+
+class Warehouse(Base):
+    __tablename__ = 'warehouses'
