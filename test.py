4bd46c5eec2e89a1c18c692dc7813ecff96d1121
“Amidou <amidou.zabre@gmail.com>
2024-09-14 09:58:36 +0000

    Connexion to db

--- test.py
+++ test.py
@@ -3,3 +3,9 @@ from sqlalchemy import create_engine
 db_path = 'sqlite:///test_alchemy.db'
 
 engine = create_engine(db_path)
+
+try:
+    conn = engine.connect()
+    print("Success")
+except Exception as ex:
+    print(ex)
